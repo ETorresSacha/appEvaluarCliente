446d64b7ed6458d73fc0c8a4fd58bbb1d8475ccb
ETorresSacha <eriktorressacha@gmail.com>
2023-11-09 19:28:52 -0500

    datos

--- src/components/dataCustomer/DataCustomer.jsx
+++ src/components/dataCustomer/DataCustomer.jsx
@@ -2,17 +2,32 @@ import React from "react";
 import { Button, Icon, Input } from "@rneui/themed";
 import { View, Text, StyleSheet, Alert, TextInput } from "react-native";
 
+const data = [
+  { title: "Nombre" },
+  { title: "Apellido" },
+  { title: "DNI" },
+  { title: "Correo" },
+  { title: "Dirección" },
+  { title: "Celular" },
+];
+
 const DataCustomer = () => {
   return (
     <View>
       <Text>DATOS</Text>
-      <View>
-        <View>
-          <Text>Nombre</Text>
-        </View>
-        <View>
-          <TextInput style={styles.input} placeholder="Nombre" />
-        </View>
+      <View style={styles.containerInput}>
+        {data?.map((element, index) => {
+          return (
+            <View key={index}>
+              <View>
+                <Text style={styles.title}>{element.title}</Text>
+              </View>
+              <View>
+                <TextInput style={styles.input} placeholder={element.title} />
+              </View>
+            </View>
+          );
+        })}
       </View>
     </View>
   );
@@ -21,9 +36,20 @@ const DataCustomer = () => {
 export default DataCustomer;
 
 const styles = StyleSheet.create({
+  containerInput: {
+    gap: 5,
+    display: "flex",
+    paddingHorizontal: 5,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 15,
+  },
   input: {
     height: 30,
-    width: 150,
+    width: 180,
     marginTop: 5,
     borderWidth: 1,
     borderRadius: 5,
