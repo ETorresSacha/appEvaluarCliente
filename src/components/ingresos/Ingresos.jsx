5a85f4c7927a254a343c8bd3760c67ca07dc7698
ETorresSacha <eriktorressacha@gmail.com>
2023-11-09 20:03:33 -0500

    ingresos

--- src/components/ingresos/Ingresos.jsx
+++ src/components/ingresos/Ingresos.jsx
@@ -7,13 +7,42 @@ import {
   StyleSheet,
   Image,
 } from "react-native";
+import { Button, Icon } from "@rneui/themed";
 
 const Ingresos = () => {
   return (
-    <View>
-      <Text>Ingresos</Text>
+    <View style={styles.conteiner}>
+      <View style={styles.leftConteiner}>
+        <Text style={styles.text}>Ingresos</Text>
+      </View>
+      <View style={styles.rightConteiner}>
+        <Button
+          icon={<Icon name="add-circle-outline" color="#FFF" />}
+          radius="lg"
+          color="#4ecb71"
+        ></Button>
+      </View>
     </View>
   );
 };
 
 export default Ingresos;
+
+const styles = StyleSheet.create({
+  conteiner: {
+    flexDirection: "row",
+    alignContent: "center",
+    marginVertical: 24,
+  },
+  leftConteiner: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  rightConteiner: {
+    flex: 1,
+    alignItems: "flex-end",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
