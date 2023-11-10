ac799ba023dfa7371a4a7cabdd998431261c06b2
ETorresSacha <eriktorressacha@gmail.com>
2023-11-09 19:47:24 -0500

    carpetas

---
+++ src/components/ingresos/Ingresos.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  View,
+  Modal,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+
+const Ingresos = () => {
+  return (
+    <View>
+      <Text>Ingresos</Text>
+    </View>
+  );
+};
+
+export default Ingresos;
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import Header from "../../components/header/Header";
 import DataCustomer from "../../components/dataCustomer/DataCustomer";
+import Gastos from "../../components/Gastos/Gastos";
+import Ingresos from "../../components/ingresos/Ingresos";
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Header />
       <DataCustomer />
+      <Ingresos />
+      <Gastos />
     </View>
   );
 };
@@ -18,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 12,
-    backgroundColor: "red",
   },
   title: {
     fontSize: 16,
