cbdedf86087b2a0735b5aa394b0f1ffc873a11cc
ETorresSacha <eriktorressacha@gmail.com>
2024-01-19 21:48:07 -0500

    clientes cancelados

--- src/views/canceledCustomer/CanceledCustomer.jsx
+++ src/views/canceledCustomer/CanceledCustomer.jsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import Header from "../../components/header/Header";
+import NavBar from "../../components/navBar/NavBar";
 
+const img =
+  "https://i.pinimg.com/originals/fe/6f/35/fe6f35a1ceedf8421c5fd776390bee12.jpg";
 const CanceledCustomer = () => {
   return (
-    <View>
-      <Text>CanceledCustomer</Text>
+    <View style={styles.container}>
+      <Image source={{ uri: img }} style={[StyleSheet.absoluteFill]}></Image>
+      <Header title={"Clientes Cancelados"} />
     </View>
   );
 };
 
 export default CanceledCustomer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(31, 36, 36)",
+  },
+});
