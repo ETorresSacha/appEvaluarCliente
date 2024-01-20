ab41019dd7590fc739a61fbe2b42e1ba6de57b4c
ETorresSacha <eriktorressacha@gmail.com>
2024-01-20 06:37:51 -0500

    customer canceled

--- src/views/canceledCustomer/CanceledCustomer.jsx
+++ src/views/canceledCustomer/CanceledCustomer.jsx
@@ -1,11 +1,52 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header/Header";
 import NavBar from "../../components/navBar/NavBar";
+import UseStorage from "../../components/hooks/UseHookStorage";
+import { customerData } from "../../utils/thunks/Thunks";
+import { format } from "date-fns";
+import { useFocusEffect } from "@react-navigation/native";
 
 const img =
   "https://i.pinimg.com/originals/fe/6f/35/fe6f35a1ceedf8421c5fd776390bee12.jpg";
 const CanceledCustomer = () => {
+  const [customer, SetCustomer] = useState([]);
+  const [day, setDay] = useState("");
+  const { onGetCronograma } = UseStorage();
+  const [data, setData] = useState({
+    dataResult: [],
+    dataResultCopy: [],
+  });
+  // Trae los datos del local storage
+  const loadCustomer = async () => {
+    try {
+      const resultCustomer = await onGetCronograma();
+      console.log(resultCustomer);
+      //   setData({
+      //     ...data,
+      //     dataResult: resultCustomer,
+      //     dataResultCopy: resultCustomer,
+      //   });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //loadCustomer();
+
+  // cargar los clientes que cancelaron su deuda
+  const resultCanceledCustomer = () => {
+    setDay(format(new Date(), "yyyy-MM-dd"));
+    let result = customerData(data.dataResult, day);
+
+    SetCustomer(result?.resultCustomerCancelled);
+  };
+  useFocusEffect(
+    React.useCallback(() => {
+      resultCanceledCustomer();
+      //return () => unsubscribe();
+    }, [])
+  );
+  console.log(customer);
   return (
     <View style={styles.container}>
       <Image source={{ uri: img }} style={[StyleSheet.absoluteFill]}></Image>
@@ -22,3 +63,5 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(31, 36, 36)",
   },
 });
+
+//! FALTA DETERMINAR DE DONDE TRAES LOS CLIENTES QUE YA PAGARON
