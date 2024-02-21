6ac3863ed0e03bd104ff020ea9cbc22deb074eca
ETorresSacha <eriktorressacha@gmail.com>
2024-02-21 17:07:50 -0500

    modaldate

--- src/components/modalDate/ModalDate.jsx
+++ src/components/modalDate/ModalDate.jsx
@@ -1,24 +1,6 @@
-import React, { useState } from "react";
-import { Calendar } from "react-native-calendars";
-import {
-  compareAsc,
-  format,
-  add,
-  formatDistance,
-  getDate,
-  isFuture,
-  isEqual,
-  differenceInDays,
-} from "date-fns";
-import {
-  View,
-  Modal,
-  StyleSheet,
-  SafeAreaView,
-  TouchableOpacity,
-  Button,
-  Text,
-} from "react-native";
+import React from "react";
+import { format } from "date-fns";
+import { View, StyleSheet } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 const ModalDate = ({
   visible,
@@ -28,13 +10,9 @@ const ModalDate = ({
   typeDatePrestamo,
 }) => {
   // Setea los datos del préstamo
-
-  const [date, setDate] = useState(new Date());
-
   const onChange = (event, selectedDate) => {
     setShowModal(false);
     const currentDate = selectedDate;
-    setDate(currentDate);
     setPrestamo({
       ...prestamo,
       [typeDatePrestamo]: format(currentDate, "yyyy-MM-dd"),
@@ -42,49 +20,22 @@ const ModalDate = ({
   };
 
   return (
-    <Modal
-      visible={visible}
-      onRequestClose={() => onClose()}
-      transparent
-      animationType="slide"
-      onBackdropP={() => setShowModal(false)}
-    >
+    <View>
       {visible && (
         <DateTimePicker
           testID="dateTimePicker"
-          value={date}
+          value={new Date()}
           mode="date"
           is24Hour={true}
           onChange={onChange}
         />
       )}
-    </Modal>
+    </View>
   );
 };
 
 export default ModalDate;
 
-const styles = StyleSheet.create({
-  conteiner: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
-  },
-  content: {
-    width: "75%",
-    backgroundColor: "#FFF",
-    padding: 18,
-    borderRadius: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-});
-//! primero ver si el modal ya no se usaria, optimizar el codigo
+const styles = StyleSheet.create({});
+
 //! AVERIGUA OVERLAY DE react-native-elements
