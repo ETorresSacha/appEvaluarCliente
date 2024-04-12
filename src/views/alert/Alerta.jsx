f5953017f70947f8f85cf25a102cdff0b1a55169
ETorresSacha <eriktorressacha@gmail.com>
2024-04-11 22:46:58 -0500

    borrado de log

--- src/views/alert/Alerta.jsx
+++ src/views/alert/Alerta.jsx
@@ -1,7 +1,5 @@
-import { StyleSheet, Text, View, TextInput, Switch, Alert } from "react-native";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import * as Notifications from "expo-notifications";
-import * as Device from "expo-device";
 import registerForPushNotificationsAsync from "./getToken";
 
 Notifications.setNotificationHandler({
@@ -67,44 +65,6 @@ const Alerta = ({ dataRed, dataGreen }) => {
       setExpoPushToken(token)
     );
   }, []);
-
-  // async function registerForPushNotificationsAsync() {
-  //   let token;
-
-  //   if (Platform.OS === "android") {
-  //     await Notifications.setNotificationChannelAsync("default", {
-  //       name: "default",
-  //       importance: Notifications.AndroidImportance.MAX,
-  //       vibrationPattern: [0, 250, 250, 250],
-  //       lightColor: "#FF231F7C",
-  //     });
-  //   }
-
-  //   if (Device.isDevice) {
-  //     const { status: existingStatus } =
-  //       await Notifications.getPermissionsAsync();
-  //     let finalStatus = existingStatus;
-  //     if (existingStatus !== "granted") {
-  //       const { status } = await Notifications.requestPermissionsAsync();
-  //       finalStatus = status;
-  //     }
-  //     if (finalStatus !== "granted") {
-  //       alert("Failed to get push token for push notification!");
-  //       return;
-  //     }
-  //     // Learn more about projectId:
-  //     // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-  //     token = (
-  //       await Notifications.getExpoPushTokenAsync({
-  //         projectId: "553ec5b6-a8cb-413a-a68c-6698a073d3ac",
-  //       })
-  //     ).data;
-  //   } else {
-  //     alert("Must use physical device for Push Notifications");
-  //   }
-
-  //   return token;
-  // }
 };
 
 export default Alerta;
