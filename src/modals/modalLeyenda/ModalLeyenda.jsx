cf9be09537ac9851b73c26e797f59e3dd2bfd98d
ETorresSacha <eriktorressacha@gmail.com>
2024-04-18 23:02:36 -0500

    nuevo modal

---
+++ src/modals/modalLeyenda/ModalLeyenda.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ModalLeyenda = () => {
+  return (
+    <View>
+      <Text>ModalLeyenda</Text>
+    </View>
+  );
+};
+
+export default ModalLeyenda;
+
+const styles = StyleSheet.create({});
