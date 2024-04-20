2db2a1f891221ae6045a14a569ccaf3907dcbea5
ETorresSacha <eriktorressacha@gmail.com>
2024-04-20 11:55:01 -0500

    thunks

--- src/modals/modalLeyenda/ModalLeyenda.jsx
+++ src/modals/modalLeyenda/ModalLeyenda.jsx
@@ -7,8 +7,63 @@ import {
 } from "react-native";
 import React from "react";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { Pie } from "react-chartjs-2";
+import { getDataColors } from "./thunks";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+//import { PieChart } from "react-native-svg-charts";
+//! aquis
+const renderChart = () => {
+  // new Chart("modelsChart", { type: "doughnut", data });
+};
+// const data = [
+//   {
+//     key: 1,
+//     value: 30,
+//     svg: { fill: "orange" },
+//     arc: { outerRadius: "110%", padAngle: 0.02 },
+//   },
+//   {|
+//     key: 2,
+//     value: 70,
+//     svg: { fill: "skyblue" },
+//     arc: { outerRadius: "100%", padAngle: 0.02 },
+//   },
+// ];
+
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const data = {
+//   labels: ["uno", "dos", "tres"],
+//   datasets: [
+//     {
+//       data: [10, 20, 30],
+//       // borderColor: getDataColors(),
+//       // backgroundColor: getDataColors(20),
+//     },
+//   ],
+// };
+
+// const opciones = {
+//   // plugins: {
+//   //   Legend: { position: "left" },
+//   // },
+//   responsive: true,
+// };
 
 const ModalLeyenda = ({ isVisible, setIsVisible }) => {
+  const data = {
+    labels: ["uno", "dos", "tres"],
+    datasets: [
+      {
+        data: [10, 20, 30],
+        // borderColor: getDataColors(),
+        // backgroundColor: getDataColors(20),
+      },
+    ],
+  };
   return (
     <Modal
       style={styles.container}
@@ -21,7 +76,21 @@ const ModalLeyenda = ({ isVisible, setIsVisible }) => {
       </TouchableWithoutFeedback>
       <View style={styles.modalContent}>
         <View>
-          <Text>LEYENDA</Text>
+          <Text>INFORMACION</Text>
+        </View>
+        {/* <Pie data={data} /> */}
+        <View style={styles.leftContainer}>
+          {/* <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <PieChart
+              style={{ height: 200, width: 200 }}
+              data={data}
+              innerRadius={50}
+              outerRadius={"80%"}
+              padAngle={0.04}
+            />
+          </View> */}
         </View>
 
         <View style={styles.leyendaIcono}>
@@ -60,5 +129,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+  },
+  leftContainer: {
+    flex: 1,
   },
 });
