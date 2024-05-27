64cd1ba771b4c19856c77b9bd2cfcaa06faeffea
ETorresSacha <eriktorressacha@gmail.com>
2024-05-26 19:05:45 -0500

    opcones del export e import

--- src/modals/modalOptionsCustomer/ModalOptionsCustomer.jsx
+++ src/modals/modalOptionsCustomer/ModalOptionsCustomer.jsx
@@ -3,15 +3,19 @@ import {
   StyleSheet,
   Text,
   View,
-  Share,
-  Pressable,
   TouchableWithoutFeedback,
+  TouchableOpacity,
 } from "react-native";
 import React, { useState } from "react";
 import ExportarData from "./ExportarData";
 import ImportarData from "./ImportarData";
 
-const optionsData = ["Exportar Data", "Importar Data"];
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const optionsData = [
+  { name: "Exportar Data", symbolName: "database-export" },
+  { name: "Importar Data", symbolName: "database-import" },
+];
 
 const ModalOptionsCustomer = ({ visible, setIsVisible }) => {
   const [exportData, setExportData] = useState(false);
@@ -42,9 +46,18 @@ const ModalOptionsCustomer = ({ visible, setIsVisible }) => {
         <View style={styles.modalContent}>
           {optionsData.map((element, index) => {
             return (
-              <Pressable onPress={() => options(element)} key={index}>
-                <Text style={styles.text}>{element}</Text>
-              </Pressable>
+              <TouchableOpacity
+                onPress={() => options(element.name)}
+                key={index}
+                style={styles.optionsDataStyle}
+              >
+                <MaterialCommunityIcons
+                  name={element.symbolName}
+                  size={40}
+                  color="rgb(36, 224, 221)"
+                />
+                <Text style={styles.text}>{element.name}</Text>
+              </TouchableOpacity>
             );
           })}
         </View>
@@ -64,15 +77,26 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   modalContent: {
-    backgroundColor: "beige",
-    borderRadius: 2,
+    backgroundColor: "rgba(16, 18, 20, 0.936)",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "white",
+    padding: 5,
     position: "absolute",
     top: "1%",
     right: "1%",
   },
   text: {
-    fontSize: 17,
-    color: "black",
-    margin: 10,
+    fontSize: 12,
+    color: "white",
+    alignSelf: "flex-end",
+  },
+  optionsDataStyle: {
+    borderRadius: 2,
+    borderColor: "white",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingTop: 5,
   },
 });
